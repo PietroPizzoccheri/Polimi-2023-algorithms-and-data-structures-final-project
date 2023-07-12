75e6d0fb94ec1b972acab043ec49ea7abbe6a4ad
PietroPizzoccheri <pietro.pizzoccheri@mail.polimi.it>
2023-07-12 17:45:04 +0200

    journey start

--- Project.c
+++ Project.c
@@ -1,1 +1,15 @@
-//aaaaaaaaabbbbbbbSSSSSSUUUUUUU
+//Prova finale API PIZZOCCHERI 10797420
+
+// librerie di libc
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <iso646.h>
+#include <limits.h>
+#include <math.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
